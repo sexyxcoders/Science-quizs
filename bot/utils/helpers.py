1fb87becff4d05dfe66cf172150a1e41ca578839
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-11-16 14:26:36 +0530

    Update helpers.py

--- bot/utils/helpers.py
+++ bot/utils/helpers.py
@@ -1,1 +1,18 @@
+# bot/utils/helpers.py
+import random
 
+def shuffle_options(options):
+    """
+    Shuffle the order of options for a question.
+    """
+    random.shuffle(options)
+    return options
+
+def format_question(q):
+    """
+    Format a question dictionary to a string.
+    """
+    text = f"❓ {q['question']}\n"
+    for idx, opt in enumerate(q['options'], start=1):
+        text += f"{idx}. {opt}\n"
+    return text
